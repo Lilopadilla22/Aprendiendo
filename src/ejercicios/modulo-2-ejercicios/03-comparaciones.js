3f822d939f5672509da46723dd7681e63bd1201e
LiliaPadilla22 <lilopadilla22@gmail.com>
2026-02-17 14:45:55 -0500

    feat: agregar estructura de ejercicios para modulo 2

---
+++ src/ejercicios/modulo-2-ejercicios/03-comparaciones.js
@@ -0,0 +1,9 @@
+/*
+EJERCICIO 3 - COMPARACIONES
+
+1️⃣ Compara el número 10 con el string "10" usando ==.
+2️⃣ Luego compáralos usando ===.
+3️⃣ Imprime ambos resultados.
+
+Explica en un comentario por qué son diferentes.
+*/
