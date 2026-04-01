be8444f06f8e7c036322f83911091fb73003548b
WilliamSalcedo <wsl_1994@hotmail.com>
2026-04-01 15:13:39 -0500

    feat se agrega solucion de ejercicio #2

--- src/ejercicios/modulo-3-ejercicios/02-else-if.js
+++ src/ejercicios/modulo-3-ejercicios/02-else-if.js
@@ -14,5 +14,14 @@ Evaluar múltiples condiciones.
 Aprobado
 */
 
-
 // ✍️ Escribe tu solución aquí 👇
+
+var nota = 75;
+
+if (nota >= 90) {
+  console.log("Excelente");
+} else if (nota >= 70) {
+  console.log("Aprobado");
+} else {
+  console.log("Reprobado");
+}
