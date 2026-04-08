e78f63d55a48a1a15eb6755c28e54420e81938be
LiliaPadilla22 <lilopadilla22@gmail.com>
2026-04-08 18:13:09 -0500

    update status

--- src/data/modulesStatus.ts
+++ src/data/modulesStatus.ts
@@ -4,5 +4,8 @@ import type { ModuleStatus } from "../types/modules";
 export const modulesStatus: Record<number, ModuleStatus> = {
   1: "completed",
   2: "completed",
-  3: 'in-progress'
+  3: "completed",
+  4: "in-progress",
+  5: "in-progress",
+  6: "in-progress"
 };
