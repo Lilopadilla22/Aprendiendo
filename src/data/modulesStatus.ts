a63b34aedfb88be95b9680136bc869d6bff45795
LiliaPadilla22 <lilopadilla22@gmail.com>
2026-04-08 18:35:35 -0500

    update status

--- src/data/modulesStatus.ts
+++ src/data/modulesStatus.ts
@@ -7,5 +7,10 @@ export const modulesStatus: Record<number, ModuleStatus> = {
   3: "completed",
   4: "in-progress",
   5: "in-progress",
-  6: "in-progress"
+  6: "in-progress",
+  7: "locked",
+  8: "locked",
+  9: "locked",
+  10: "locked",
+  11: "locked"
 };
